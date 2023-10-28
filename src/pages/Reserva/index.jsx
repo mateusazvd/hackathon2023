5bf0f444e9afb763b89cbd70845e6e4817dab40d
HENDERSON VICENTE RODRIGUES JUCA LIMA <125266@pmenos.com.br>
2023-10-28 09:07:14 -0300

    pagina de reserva de veiculo

--- src/pages/Reserva/index.jsx
+++ src/pages/Reserva/index.jsx
@@ -1,7 +1,14 @@
+import { Container } from "./styles";
+
 export function Reserva(){
     return(
-        <>
-            Reserva
-        </>    
+        <Container>  
+            <h1>Reserva de veículo</h1>
+            <p>Data e Hora Atuak</p>
+            <label id="placa">Informe a placa do veículo:</label>
+            <input type="text" id="placa"/>
+            <p>Infos Veiculo</p>
+            <button>Iniciar Reserva/Finalizar Reserva</button>
+        </Container>    
     )
 }
---
+++ src/pages/Reserva/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+width: 100%;
+height: 100vh;
+background-color: #000;
+color:#fff;
+position: fixed;
+top: 0;
+left: 0;
+z-index: 999; 
+`
