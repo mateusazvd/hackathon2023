71dff5ee7c7da4959e3a4177f580b776f0757bea
Abner Costa <abnermoura@pmenos.com.br>
2023-10-28 17:12:28 -0300

    feat/GerarRelatorioPage

--- src/pages/GerarRelatorio/index.jsx
+++ src/pages/GerarRelatorio/index.jsx
@@ -1,7 +1,40 @@
-export function GerarRelatorio(){
-    return(
-        <>
-            gerar relatorio   
-        </>    
-    )
+import "./style.css";
+import { multas } from "../../Utils/multas";
+import ResumoMulta from "../../components/resumoMulta";
+
+export default function GerarRelatorio() {
+  return (
+    <>
+      <div className="main-container-desktop">
+        <div className="secundary-container">
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div className="titleContainer">
+              <h1 style={{ color: "white" }}>Gerar relatório</h1>
+              <p>Crie o relatório detalhado das últmas multas de trânsito</p>
+            </div>
+            <div className="containerUpload">
+              <button className="btnUpload">Adicionar arquivo</button>
+            </div>
+          </div>
+
+          <input type="text" className="inpSearch" placeholder="Pesquisar" />
+          <button className="btnSearch">Pesquisar</button>
+          <div className="containerMulta">
+            {multas.map((e) => (
+              <ResumoMulta
+                codigomulta={e.codigomulta}
+                placaveiculo={e.placaveiculo}
+                localizacao={e.localizacao}
+                datamulta={e.datamulta}
+                gravidademulta={e.gravidademulta}
+                matriculacondutor={e.matriculacondutor}
+                nomecondutor={e.nomecondutor}
+                motivomulta={e.motivomulta}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
